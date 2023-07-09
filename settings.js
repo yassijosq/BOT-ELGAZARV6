a0b66a26e9574b195507c7f464b1d56d1e650a59
Yasser <136765083+yassijosq@users.noreply.github.com>
2023-07-09 22:52:43 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
+global.owner = ['212645866783'] //ur owner number
+global.ownername = "ᴛᴏᴍʀᴛ❤️" //ur owner name
+global.ytname = "YT: ᴛᴏᴍʀᴛ" //ur yt chanel name
+global.socialm = "GitHub: ᴛᴏᴍʀᴛ" //ur github or insta name
+global.location = "Africa, Morroco, Casablanca" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.ownernomer = "212645866783" //ur number
+global.premium = ['212645866783'] //ur premium number
+global.botname = '❤️ʜ4xx ᗷOT❤️' //ur bot name
+global.linkz = "WA.ME\+212777053659" //your theme url which will be displayed on whatsapp
+global.websitex = "https://i.postimg.cc/xjRsNfsn/Anyapic.jpg" //ur website to be displayed
+global.botscript = 'WA.ME\+212777053659' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "⍣اʜ4xx⍣" //ur sticker watermark packname
+global.author = "⍣21277703659⍣" //ur sticker watermark author
+global.wm = "❤️ʜ4xx-ʙʏ-ᴛᴏᴍʀᴛ❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
